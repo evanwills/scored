838bfa52353c319d481a6952e695bd989abe70d0
Evan Wills <evan.wills@acu.edu.au>
2020-02-10 09:37:44 +1100

    start adding state machine management to Redux state.

--- src/state/game/game.initial-state.ts
+++ src/state/game/game.initial-state.ts
@@ -1,4 +1,4 @@
-import { IGameActive, END_MODE, PLAY_ORDER, IPause, IConfigGame } from '../utilities/types'
+import { IGameActive, END_MODE, PLAY_ORDER, IPause, IConfigGame, IFiniteStateMachine, GAME_STATE, AllowableStates } from '../utilities/types'
 import { initialRound } from '../round/round.initital-states'
 
 
@@ -35,5 +35,100 @@ export const initialGame : IGameActive = {
   },
   round: initialRound,
   scores: [],
-  start: -1
+  start: -1,
+  state: GAME_STATE.NO_GAME
+}
+
+/**
+ * All of the possible states a game can be in plus their
+ * allowable previous and next states
+ */
+export const gameFiniteStateMachine : IFiniteStateMachine = {
+  NO_GAME: {
+    previous: [
+      GAME_STATE.GAME_STORED
+    ],
+    next: [
+      GAME_STATE.CHOOSING_GAME
+    ],
+    hasSubState: false
+  },
+  CHOOSING_GAME: {
+    previous: [
+      GAME_STATE.NO_GAME,
+      GAME_STATE.GAME_STORED
+    ],
+    next: [
+      GAME_STATE.MANAGE_PLAYERS
+    ],
+    hasSubState: false
+  },
+  GAME_INITIALISED: {
+    previous: [
+      GAME_STATE.GAME_STORED,
+      GAME_STATE.CHOOSING_GAME
+    ],
+    next: [
+      GAME_STATE.MANAGE_PLAYERS
+    ],
+    hasSubState: false
+  },
+  MANAGE_PLAYERS: {
+    previous: [
+      GAME_STATE.GAME_INITIALISED
+    ],
+    next: [
+      GAME_STATE.PLAYING_GAME
+    ],
+    hasSubState: true
+  },
+  PLAYING_GAME: {
+    previous: [
+      GAME_STATE.MANAGE_PLAYERS,
+      GAME_STATE.GAME_PAUSED
+    ],
+    next: [
+      GAME_STATE.GAME_PAUSED,
+      GAME_STATE.GAME_ENDED
+    ],
+    hasSubState: true
+  },
+  GAME_PAUSED: {
+    previous: [
+      GAME_STATE.PLAYING_GAME
+    ],
+    next: [
+      GAME_STATE.PLAYING_GAME
+    ],
+    hasSubState: true
+  },
+  GAME_ENDED: {
+    previous: [
+      GAME_STATE.PLAYING_GAME
+    ],
+    next: [
+      GAME_STATE.GAME_FINALISED
+    ],
+    hasSubState: false
+  },
+  GAME_FINALISED: {
+    previous: [
+      GAME_STATE.GAME_ENDED
+    ],
+    next: [
+      GAME_STATE.GAME_STORED
+    ],
+    hasSubState: false
+  },
+  GAME_STORED: {
+    previous: [
+      GAME_STATE.GAME_FINALISED
+    ],
+    next: [
+      GAME_STATE.NO_GAME,
+      GAME_STATE.CHOOSING_GAME,
+      GAME_STATE.GAME_INITIALISED
+    ],
+    hasSubState: false
+  },
 }
